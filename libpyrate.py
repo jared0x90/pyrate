41c4835a6708730a877d5ef801eba24f834f58f4
Jared De Blander <jared@deblander.org>
2015-11-21 23:49:07 -0500

    Mucking about

---
+++ libpyrate.py
@@ -0,0 +1,9 @@
+def function otp_file(filein, fileout1, fileout2):
+    message = (
+        "One time pad.\n"
+        "file in: " + filein + "\n"
+        "file out 1: " + fileout1 + "\n"
+        "file out 2: " + fileout2 + "\n"
+    )
+
+    print message
---
+++ pyrate.py
@@ -0,0 +1,4 @@
+import libpyrate
+
+def main:
+    libpyrate.otp_file('test.txt' , 'test.txt.potp1',  'test.txt.potp2')
